7b6acdecbaee560f014000bd50f91a54fa677a67
Byrdi <z.byrd@hotmail.com>
2025-12-14 21:51:10 +0100

    Update api rewrite to work on both production and developmen enviroments

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,16 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   async rewrites() {
-    // 1. Check if an Environment Variable is set. 
-    // If not, default to Localhost (for your laptop).
-    const backendUrl = process.env.BACKEND_INTERNAL_URL || 'http://127.0.0.1:8000';
-    
-    console.log(`Proxying /api requests to: ${backendUrl}`); // Helpful debug log
-
     return [
       {
         source: '/api/:path*',
-        destination: `${backendUrl}/api/:path*`,
+        // The destination now dynamically reads from an environment variable
+        destination: `${process.env.BACKEND_URL}/api/:path*`,
       },
     ]
   },
